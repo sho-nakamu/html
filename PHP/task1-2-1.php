49147b789afe0db0c1b81e5e82ec8642e50f37dc
sho-nakamu <nshogo0317@gmail.com>
2024-01-30 22:33:55 +0900

    task1-2-1完成

--- PHP/task1-2-1.php
+++ PHP/task1-2-1.php
@@ -6,10 +6,15 @@
   <title>PHP演習</title>
 </head>
 <body>
+<?php
+echo '現在消費税は10%です。<br>'
+ ?>
+  <?php
+  echo '鉛筆が100円で税込110円です。<br>'
+?>
 
 <?php
-  echo  '現在消費税は10%です。'
-  ?>
-  
+echo '消しゴムが200円で 税込み220円です'
+?>
 </body>
 </html>
