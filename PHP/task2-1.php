d4f772fbaa93031168a4cd6da5ccc160932b3119
sho-nakamu <nshogo0317@gmail.com>
2024-02-01 23:19:20 +0900

    task2-1php途中

---
+++ PHP/task2-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP練習</title>
+</head>
+<body>
+  <?php
+  <ul>
+  $a =array('日曜日','火曜日','水曜日','木曜日','金曜日','土曜日');
+  </ul>
+  ?>
+  
+</body>
+</html>
