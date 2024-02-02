57975c871d40836f2a2b7a4d0edf7e14d952f39f
sho-nakamu <nshogo0317@gmail.com>
2024-02-02 21:42:20 +0900

    2-2php途中

--- PHP/task2-1.php
+++ PHP/task2-1.php
@@ -7,10 +7,33 @@
 </head>
 <body>
   <?php
-  <ul>
-  $a =array('日曜日','火曜日','水曜日','木曜日','金曜日','土曜日');
-  </ul>
+  $weeks = array('日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日');
   ?>
+
+     <?php
+      print($weeks[0]);
+      ?>
+      <?php
+      print($weeks[1]);
+      ?>
+       <?php
+      print($weeks[2]);
+      ?>
+       <?php
+      print($weeks[3]);
+      ?>
+       <?php
+      print($weeks[4]);
+      ?>
+       <?php
+      print($weeks[5]);
+      ?>
+       <?php
+      print($weeks[6]);
+      ?>
+
+   
+ 
   
 </body>
 </html>
---
+++ PHP/task2-2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP演習</title>
+</head>
+<body>
+  <?php
+  $a =array('商品','価格','税込み価格');
+  $b = ['鉛筆','100円','110円'];
+  $c =['消しゴム','200円','220円'];
+  $d =['定規','300円',,'330円']；
+</body>
+</html>
