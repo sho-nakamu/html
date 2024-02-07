b57733744d47b6d3105b8d43297769296515b74b
sho-nakamu <nshogo0317@gmail.com>
2024-02-07 22:34:10 +0900

    2-2完成

--- PHP/task2-2.php
+++ PHP/task2-2.php
@@ -8,12 +8,33 @@
 <body>
   <?php
   $a = array('商品','価格','税込み価格');
-  $pen= array('ペン',100,110);
+  $pen= array('鉛筆',100,110);
   $kesi= array('消しゴム',200,220);
   $mono= array('定規',300,330);
-  echo'<table>';
-   echo '<th>$a[0]</th>';
-  echo'</table>';
-    ?>
+  ?>
+  
+   <table>
+  <tr>
+    <th><?= $a[0]?></th>
+    <th><?=$a[1]?></th>
+    <th><?=$a[2]?></th>
+   </tr> 
+   <tr>
+    <td><?=$pen[0]?></td>
+    <td><?=$pen[1]?></td>
+    <td><?=$pen[2]?></td>
+   </tr>
+   <tr>
+    <td><?=$kesi[0]?></td>
+    <td><?=$kesi[1]?></td>
+    <td><?=$kesi[2]?></td>
+   </tr>
+   <tr>
+    <td><?=$mono[0]?></td>
+    <td><?=$mono[1]?></td>
+    <td><?=$mono[2]?></td>
+   </tr>
+ 
+  </table>
 </body>
 </html>
