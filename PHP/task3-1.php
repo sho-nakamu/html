9204412165f6b5fc26ec63ee0a5a8743840c386e
sho-nakamu <nshogo0317@gmail.com>
2024-02-07 23:07:24 +0900

    3-1完成

--- PHP/task3-1.php
+++ PHP/task3-1.php
@@ -7,3 +7,36 @@
 </head>
 <body>
   <?php
+  $moku= array('商品','価格','税込価格','1Dzの価格');
+  $pen=  array('鉛筆');
+  $gomu= array('消しゴム');
+  $num1= 100;
+  $num2=110;
+  $num=12;
+  $num3=200;
+  $num4=220;
+  ?>
+  <table>
+    <tr>
+      <th><?=$moku[0]?></th>
+      <th><?=$moku[1]?></th>
+      <th><?=$moku[2]?></th>
+      <th><?=$moku[3]?></th>
+    </tr>
+    <tr>
+    <td><?=$pen[0]?></td>
+    <td><?=$num1?></td>
+    <td><?=$num2?></td>
+    <td><?=$num2*$num?></td>
+    </tr>
+    <tr>
+      <td><?=$gomu[0]?></td>
+      <td><?=$num3?></td>
+      <td><?=$num4?></td>
+      <td><?=$num4*$num?></td>
+
+    </tr>
+
+  </table>
+</body>
+</html>
