577b68543862b6142be5171fd99c824fb488ac4b
sho-nakamu <nshogo0317@gmail.com>
2024-02-09 23:00:47 +0900

    4-2-1　途中

---
+++ PHP/4-2-1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $a = mt_rand(1.12);
+  switch($a){
+    case $a ===12;
+    $judge ="$a perfect";
+    default $judge = $a ="月はの季節です";
+  }
+  ?>
+
+</body>
+</html>
