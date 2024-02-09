4abbf5aed4bead5786c5ed07618361110077b206
sho-nakamu <nshogo0317@gmail.com>
2024-02-09 23:38:53 +0900

    5-2途中

---
+++ PHP/5-2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $i='日';
+  while($i<='土'){
+    echo$i.'曜日<br>';
+    $i++;  
+  }
+  ?>
+</body>
+</html>
