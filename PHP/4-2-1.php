5de46c47c55aa23f09ffba1387a330859e4497bc
sho-nakamu <nshogo0317@gmail.com>
2024-02-14 21:53:55 +0900

    task4-2-1完成

--- PHP/4-2-1.php
+++ PHP/4-2-1.php
@@ -6,14 +6,32 @@
   <title>Document</title>
 </head>
 <body>
-  <?php
-  $a = mt_rand(1.12);
-  switch($a){
-    case $a ===12;
-    $judge ="$a perfect";
-    default $judge = $a ="月はの季節です";
-  }
-  ?>
-
+<?php
+$randomMonth = rand(1, 12);
+switch ($randomMonth) {
+    case 1:
+    case 2:
+    case 12:
+        echo "{$randomMonth}月は冬です。";
+        break;
+    case 3:
+    case 4:
+    case 5:
+        echo "{$randomMonth}月は春です。";
+        break;
+    case 6:
+    case 7:
+    case 8:
+        echo "{$randomMonth}月は夏です。";
+        break;
+    case 9:
+    case 10:
+    case 11:
+        echo "{$randomMonth}月は秋です。";
+        break;
+    default:
+        echo "無効な月が生成されました。";
+}
+?>
 </body>
 </html>
