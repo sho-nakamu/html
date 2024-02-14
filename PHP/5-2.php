d8cc5630aa7b60be961e95c0d935a897dc0dfae8
sho-nakamu <nshogo0317@gmail.com>
2024-02-15 00:15:10 +0900

    task5-2完成

--- PHP/5-2.php
+++ PHP/5-2.php
@@ -6,12 +6,13 @@
   <title>Document</title>
 </head>
 <body>
-  <?php
-  $i='日';
-  while($i<='土'){
-    echo$i.'曜日<br>';
-    $i++;  
-  }
-  ?>
+<?php
+$weekdays = ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日'];
+
+// 配列の要素を順に表示
+foreach ($weekdays as $day) {
+    echo $day . "<br>";
+}
+?>
 </body>
 </html>
