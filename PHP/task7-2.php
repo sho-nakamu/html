97122b6ff39a8bf31c3be245e0a2963d3bdb5ff9
sho-nakamu <nshogo0317@gmail.com>
2024-02-28 21:56:09 +0900

    task7-1..7-2完成

--- PHP/task7-2.php
+++ PHP/task7-2.php
@@ -30,7 +30,7 @@ class Person {
 }
 
 // インスタンスを作成
-$person = new Person("(0001)", 31, "", "佐藤一郎");
+$person = new Person("(S0001)", 31, "", "佐藤一郎");
 
 // プロパティにアクセスして情報を取得
 echo "名前: " . $person->getName() . "\n";
@@ -117,4 +117,84 @@ echo "名前: " . $person->getName() . "\n";
 echo "年齢: " . $person->getAge() . "\n";
 echo "性別: " . $person->getGender() . "\n";
 echo "ID番号: " . $person->getId() . "\n"
-?>
+?><br>
+<?php
+class Pe{
+    private $id;
+    private $name;
+    private $age;
+    private $gender;
+
+    public function __construct($id, $name, $ager,$gender) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->gender = $gender;
+        $this->id = $id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function getGender() {
+        return $this->gender;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+}
+
+// インスタンスを作成
+$person = new Person("(S0004)", 24, "女性", "田中友子");
+
+// プロパティにアクセスして情報を取得
+echo "名前: " . $person->getName() . "\n";
+echo "年齢: " . $person->getAge() . "\n";
+echo "性別: " . $person->getGender() . "\n";
+echo "ID番号: " . $person->getId() . "\n"
+?> <br>
+<?php
+class P{
+    private $id;
+    private $name;
+    private $age;
+    private $gender;
+
+    public function __construct($id, $name, $ager,$gender) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->gender = $gender;
+        $this->id = $id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function getGender() {
+        return $this->gender;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+}
+
+// インスタンスを作成
+$person = new P("(S0005)", 27, "男性", "中村 三郎");
+
+// プロパティにアクセスして情報を取得
+echo "名前: " . $person->getName() . "\n";
+echo "年齢: " . $person->getAge() . "\n";
+echo "性別: " . $person->getGender() . "\n";
+echo "ID番号: " . $person->getId() . "\n"
+?> 
